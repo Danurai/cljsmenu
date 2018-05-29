9df00383e2d99cf76d8aa24d8a040b6369f2704b
Danurai <Danurai@users.noreply.github.com>
2018-05-29 17:25:42 +0100

    Add files via upload

--- src/cljs/cljsmenu/core.cljs
+++ src/cljs/cljsmenu/core.cljs
@@ -26,7 +26,7 @@
 (def planets  (r/atom nil))
 (def log      (r/atom ()))
 
-(defn- dev? [] false)
+(defn- dev? [] true)
 (defn- devimg [src]
   (if (dev?) nil src))
   
@@ -38,7 +38,11 @@
 
 (defn logaction [msg]
   (swap! log conj ^{:key (gensym)}[:p.logitem msg]))
-      
+
+(defn- discard-img [plyr]
+  (let [discarded (->> @deck plyr :decklist (filter #(= (:pos %) :discard)) last)]
+    (if (nil? discarded) "/img/cardback.png" (:img discarded))))
+  
 (defn build-planets []
   (reset! planets 
     (take 7 
@@ -47,65 +51,67 @@
           (assoc p :id idx :revealed (< idx 5))) 
       (->> @cards (filter #(= (:type_code %) "planet")) shuffle)))))
 
-(defn shuffledeck []
-  (swap! deck assoc :decklist (-> @deck :decklist shuffle))
-  (logaction "Shuffle Deck."))      
+(defn shuffledeck [plyr]
+  (swap! deck assoc-in [plyr :decklist] (-> @deck plyr :decklist shuffle))
+  (logaction [:span [:b plyr] ": Shuffle Deck."]))      
   
-(defn card-draw []
-  (let [drawid (->> @deck :decklist (filter #(= (:pos %) :deck)) first :idx)]
-    (swap! deck assoc :decklist (map #(if (= (:idx %) drawid) (assoc % :pos :hand) %) (:decklist @deck)))
-    (logaction "Draw Card.")))
+(defn card-draw [plyr]
+  (let [drawid (->> @deck plyr :decklist (filter #(= (:pos %) :deck)) first :idx)]
+    (swap! deck assoc-in [plyr :decklist] (map #(if (= (:idx %) drawid) (assoc % :pos :hand) %) (-> @deck plyr :decklist)))
+    (logaction [:span [:b plyr] ": Draw Card."])))
 
     
-(defn build-deck [rawdeck]
+(defn build-deck [plyr rawdeck]
   (let [all-cards (->> (map (fn [x] (repeat (x rawdeck) (getcard (name x)))) (keys rawdeck)) (reduce concat))]
-    (reset! deck {:warlord (->> all-cards (filter #(= (:type_code %) "warlord_unit")) first)
-                :resources (->> all-cards (filter #(= (:type_code %) "warlord_unit")) first :starting_resources)
-                :hp (->> all-cards (filter #(= (:type_code %) "warlord_unit")) first :hp)
-                :bloodied false
-                :decklist (map-indexed #(assoc %2 :idx %1 :pos :deck) (filter #(not= (:type_code %) "warlord_unit") all-cards))})
-    (shuffledeck)
-    (dotimes [n (-> @deck :warlord :starting_hand)] (card-draw))))
+    (swap! deck assoc plyr {:warlord (->> all-cards (filter #(= (:type_code %) "warlord_unit")) first)
+                           :resources (->> all-cards (filter #(= (:type_code %) "warlord_unit")) first :starting_resources)
+                           :hp (->> all-cards (filter #(= (:type_code %) "warlord_unit")) first :hp)
+                           :bloodied false
+                           :decklist (map-indexed #(assoc %2 :idx %1 :pos :deck) (filter #(not= (:type_code %) "warlord_unit") all-cards))})
+    (shuffledeck plyr)
+    (dotimes [n (-> @deck plyr :warlord :starting_hand)] (card-draw plyr))))
 
 (defn do-setup  []
   (go 
     (reset! cards (:data (js->clj (.parse js/JSON (<! (GET "http://localhost:9009/api/data/cards"))) :keywordize-keys true)))
     (build-planets)
-    (build-deck  {:010006 1 :010123 4 :010124 1 :010125 2 :010126 1 :010128 1 :010127 1 :010137 1 :010129 2 :010133 1 :010134 2 :010136 1 :010131 1 :010138 1 :010135 1 :010132 1 :010130 1 :010171 1 :010170 1 :010174 1 :010172 1 :010169 1 :010173 1 :010143 1 :010145 1 :010144 1 :010139 1 :010140 1 :010142 1 :010141 1})
+    (build-deck :p1 {:010006 1 :010123 4 :010124 1 :010125 2 :010126 1 :010128 1 :010127 1 :010137 1 :010129 2 :010133 1 :010134 2 :010136 1 :010131 1 :010138 1 :010135 1 :010132 1 :010130 1 :010171 1 :010170 1 :010174 1 :010172 1 :010169 1 :010173 1 :010143 1 :010145 1 :010144 1 :010139 1 :010140 1 :010142 1 :010141 1})
+    (build-deck :p2 {:010001 1 :010008 4 :010009 1 :010010 2 :010011 1 :010012 1 :010170 1 :010019 1 :010171 1 :010022 1 :010017 1 :010021 1 :010014 1 :010020 1 :010023 1 :010016 1 :010015 2 :010027 1 :010028 1 :010174 1 :010024 1 :010026 1 :010172 1 :010025 1 :010169 1 :010029 1 :010030 1 :010173 1 :010013 2 :010018 1})
     (reset! log ())
 ))
 
-(defn hq-phase []
+(defn hq-phase [plyr]
   ; assign next First Planet - in code
   ; Reveal next planet - in code
   ; Draw 2 cards
-  (dotimes (n 2) (card-draw))
+  (dotimes (n 2) (card-draw plyr))
   ; Take 4 Resources
-  (swap! deck update :resources + 4)
+  (swap! deck update-in [plyr :resources] + 4)
   ; Ready all exhausted cards
-  (swap! deck assoc :decklist (map #(dissoc % :exhausted) (-> @deck :decklist)))
+  (swap! deck assoc-in [plyr :decklist] (map #(dissoc % :exhausted) (-> @deck plyr :decklist)))
   ; Pass Initiative
   )
 
-(defn card-discard [idx]
-  (swap! deck assoc :decklist (map #(if (= (:idx %) idx) (assoc % :pos :discard) %)
-                              (-> @deck :decklist))))
+(defn card-discard [plyr idx]
+  (swap! deck assoc-in [plyr :decklist] (map #(if (= (:idx %) idx) (assoc % :pos :discard) %)
+                              (-> @deck plyr :decklist))))
                               
-(defn card-deploy [card-id planet-id]
-  (let [crd (->> @deck :decklist (filter #(= (:idx %) card-id)) first)
+(defn card-deploy [plyr card-id planet-id]
+  (let [crd (->> @deck plyr :decklist (filter #(= (:idx %) card-id)) first)
        plnt (->> @planets (filter #(= (:id %) planet-id)) first)]
-    (swap! deck update :resources - (:cost crd 0))
-    (swap! deck assoc :decklist (map #(if (= (:idx %) card-id) (assoc % :pos planet-id) % ) (:decklist @deck)))
-    (logaction [:span [:a {:href "#" :on-mouse-over #(swap! appstate assoc :img (:img crd))} (:name crd)] (str " deployed to " (if (some? plnt) (:name plnt) "HQ"))])))
+    (swap! deck update-in [plyr :resources] - (:cost crd 0))
+    (swap! deck assoc-in [plyr :decklist] (map #(if (= (:idx %) card-id) (assoc % :pos planet-id) % ) (-> @deck plyr :decklist)))
+    (logaction [:span [:b plyr] ": " [:a {:href "#" :on-mouse-over #(swap! appstate assoc :img (:img crd))} (:name crd)] (str " deployed to " (if (some? plnt) (:name plnt) "HQ"))])))
 
-(defn card-play [card-id]
-  (let [crd (->> @deck :decklist (filter #(= (:idx %) card-id)) first)]
-    (swap! deck update :resources - (:cost crd 0))
-    (card-discard card-id)
-    (logaction [:span "Played " [:a {:href "#" :on-mouse-over #(swap! appstate assoc :img (:img crd))} (:name crd)]])))
+(defn card-play [plyr card-id]
+  (let [crd (->> @deck plyr :decklist (filter #(= (:idx %) card-id)) first)]
+    (swap! deck update-in [plyr :resources] - (:cost crd 0))
+    (card-discard plyr card-id)
+    (logaction [:span [:b plyr] " played " [:a {:href "#" :on-mouse-over #(swap! appstate assoc :img (:img crd))} (:name crd)]])))
     
-(defn card-toggle-exhaust [card-id] 
-  (swap! deck assoc :decklist (map #(if (= (:idx %) card-id) (assoc % :exhausted (-> % :exhausted true? not)) %) (@deck :decklist) )))
+(defn card-toggle-exhaust [plyr card-id] 
+  (swap! deck assoc-in [plyr :decklist] 
+    (map #(if (= (:idx %) card-id) (assoc % :exhausted (-> % :exhausted true? not)) %) (-> @deck plyr :decklist) )))
   
 (defn toggle-popupmenu [e]
   (swap! menu assoc :hidden (->> @menu :hidden ((complement true?)))
@@ -122,24 +128,24 @@
 (defn discard-click [e]
   (prn e))
   
-(defn hand-click [idx e]
-  (let [card (->> @deck :decklist (filter #(= (:idx %) idx)) first)]
-    (when (<= (:cost card 0) (:resources @deck))
+(defn hand-click [plyr idx e]
+  (let [card (->> @deck plyr :decklist (filter #(= (:idx %) idx)) first)]
+    (when (<= (:cost card 0) (-> @deck plyr :resources))
       (swap! menu assoc :buttons
         (concat 
           (case (:type_code card) 
-            "army_unit"  (remove nil? (map #(if (:revealed %) {:title (:name %) :data-action (fn [] (card-deploy (:idx card) (:id %)))}) @planets))
-            "attachment" (remove nil? (map #(if (:revealed %) {:title (:name %) :data-action (fn [] (card-deploy (:idx card) (:id %)))}) @planets))
-            "event"     [{:title "Play" :data-action #(card-play (:idx card))}]
-            "support"   [{:title "Deploy to HQ" :data-action #(card-deploy (:idx card) -1)}]
+            "army_unit"  (remove nil? (map #(if (:revealed %) {:title (:name %) :data-action (fn [] (card-deploy plyr (:idx card) (:id %)))}) @planets))
+            "attachment" (remove nil? (map #(if (:revealed %) {:title (:name %) :data-action (fn [] (card-deploy plyr (:idx card) (:id %)))}) @planets))
+            "event"     [{:title "Play" :data-action #(card-play plyr (:idx card))}]
+            "support"   [{:title "Deploy to HQ" :data-action #(card-deploy plyr (:idx card) -1)}]
             nil)
           [{:title "divider"}
-           {:title "Discard" :data-action #(card-discard (:idx card))}]))
+           {:title "Discard" :data-action #(card-discard plyr (:idx card))}]))
       (toggle-popupmenu e))))
 
-(defn card-click [idx e]
-  (swap! menu assoc :buttons [{:title "Discard" :data-action #(card-discard idx)}
-                           {:title "Exhaust\\Ready" :data-action #(card-toggle-exhaust idx)}])
+(defn card-click [plyr idx e]
+  (swap! menu assoc :buttons [{:title "Discard" :data-action #(card-discard plyr idx)}
+                           {:title "Exhaust\\Ready" :data-action #(card-toggle-exhaust plyr idx)}])
   (toggle-popupmenu e))
 
   
@@ -160,55 +166,55 @@
               {:on-click #((:data-action button)(close-popupmenu))}
               (:title button)]))]])
 
-(defn cardimgwrap [crd]
-    ^{:key (:idx crd)}[:div.card-wrap 
-      [:img.card {:class (if (true? (:exhausted crd)) "card-tapped")
-                :src (devimg (:img crd)) 
-                :alt (:name crd)
-                :on-click #(card-click (:idx crd) %)
-                :on-mouse-over #(swap! appstate assoc :img (:img crd))}]])
+(defn cardimgwrap [plyr crd]
+    ^{:key (:idx crd)}[:div.whc-card-wrap 
+      [:img.whc-card {:class (if (true? (:exhausted crd)) "whc-card-tapped")
+                    :src (devimg (:img crd)) 
+                    :alt (:name crd)
+                    :on-click #(card-click plyr (:idx crd) %)
+                    :on-mouse-over #(swap! appstate assoc :img (:img crd))}]])
             
-(defn hq []
-  [:div#hq
+(defn hq [plyr]
+  [:div#p1hq
     [:div "HQ"]
     [:div
-      [:div#discard.card-wrap
-        [:img.card {:src (devimg nil) :alt "discard" :on-click discard-click}]
-        [:span.cardcount (->> @deck :decklist (filter #(= (:pos %) :discard)) count)]]
-      [:div#deck.card-wrap
-        [:img.card {:src (devimg "/img/cardback.png") :alt "deck" :on-click #(deck-click %)}]
-        [:span.cardcount (->> @deck :decklist (filter #(= (:pos %) :deck)) count)]]
-      [:div.card-wrap
-        [:img.card {:src (devimg (-> @deck :warlord :img)) :alt (-> @deck :warlord :name)}]] ;(-> @deck :warlord :img)}]
-      [:div.card-wrap.resources 
+      [:div#discard.whc-card-wrap
+        [:img.whc-card {:src (-> plyr discard-img devimg) :alt "discard" :on-click discard-click}]
+        [:span.whc-cardcount (->> @deck plyr :decklist (filter #(= (:pos %) :discard)) count)]]
+      [:div#deck.whc-card-wrap
+        [:img.whc-card {:src (devimg "/img/cardback.png") :alt "deck" :on-click #(deck-click %)}]
+        [:span.whc-cardcount (->> @deck plyr :decklist (filter #(= (:pos %) :deck)) count)]]
+      [:div#warlord.whc-card-wrap
+        [:img.whc-card {:src (devimg (-> @deck plyr :warlord :img)) :alt (str "Warlord: "  (-> @deck plyr :warlord :name))}]]
+      [:div.resources 
         [:div 
-          [:span.mx-2 [:i.fas.fa-cog.fa-xs.mr-2] (:resources @deck)]
-          [:span.btn-res {:on-click #(swap! deck update :resources dec)} "-"]
-          [:span.btn-res {:on-click #(swap! deck update :resources inc)} "+"]]
+          [:span.mx-2 [:i.fas.fa-cog.fa-xs.mr-2] (-> @deck plyr :resources)]
+          [:span.btn-res {:on-click #(swap! deck update-in [plyr :resources] dec)} "-"]
+          [:span.btn-res {:on-click #(swap! deck update-in [plyr :resources] inc)} "+"]]
         [:div 
-          [:span.mx-2 [:i.fas.fa-heart.fa-xs.mr-2] (:hp @deck)]
-          [:span.btn-res {:on-click #(swap! deck update :hp dec)} "-"]
-          [:span.btn-res {:on-click #(swap! deck update :hp inc)} "+"]]
+          [:span.mx-2 [:i.fas.fa-heart.fa-xs.mr-2] (-> @deck plyr :hp)]
+          [:span.btn-res {:on-click #(swap! deck update-in [plyr :hp] dec)} "-"]
+          [:span.btn-res {:on-click #(swap! deck update-in [plyr :hp] inc)} "+"]]
         [:div.m-1
           [:button.btn.btn-block.btn-outline-light.btn-sm
-            {:on-click #(card-draw)} "Draw"]]
+            {:on-click #(card-draw plyr)} "Draw"]]
         [:div.m-1
           [:button.btn.btn-block.btn-outline-light.btn-sm
-            {:on-click #(hq-phase)} "HQ Phase"]]]
-      [:div.card-wrap
+            {:on-click #(hq-phase plyr)} "HQ Phase"]]]
+      [:div.whc-card-wrap
         (doall 
-          (for [crd (->> @deck :decklist (filter #(= (:pos %) -1)))]
-            (cardimgwrap crd)))]]
-    [:div (str "Hand (" (->> @deck :decklist (filter #(= (:pos %) :hand)) count) ")")]
+          (for [crd (->> @deck plyr :decklist (filter #(= (:pos %) -1)))]
+            (cardimgwrap plyr crd)))]]
+    [:div (str "Hand (" (->> @deck plyr :decklist (filter #(= (:pos %) :hand)) count) ")")]
     [:div
-      (doall (for [r (->> @deck :decklist (filter #(= (:pos %) :hand)))]
+      (doall (for [r (->> @deck plyr :decklist (filter #(= (:pos %) :hand)))]
         ^{:key (:idx r)}
-          [:div.card-wrap {:class (if (<= (:cost r 0) (-> @deck :resources)) "can-play" "no-play") }
-            [:img.card {:src (devimg (:img r))
-                        :alt (:name r)
-                        :on-click #(hand-click (:idx r) %)
-                        ;:on-mouse-out #(swap! appstate assoc :img nil)
-                        :on-mouse-over #(swap! appstate assoc :img (:img r))}]]))]  ;(:img r)
+          [:div.whc-card-wrap {:class (if (<= (:cost r 0) (-> @deck plyr :resources)) "can-play" "no-play") }
+            [:img.whc-card {:src (devimg (:img r))
+                          :title (:name r)
+                          :on-click #(hand-click plyr (:idx r) %)
+                          ;:on-mouse-out #(swap! appstate assoc :img nil)
+                          :on-mouse-over #(swap! appstate assoc :img (:img r))}]]))]  ;(:img r)
   ])
   
 (defn board-planets []
@@ -217,20 +223,26 @@
         ^{:key (:id p)}
           [:div.row.my-2
           ; P1 deployment
-            [:div.col-sm-4
-              (for [crd (->> @deck :decklist (filter #(= (:pos %) (:id p))))]
-                (cardimgwrap crd))
+            [:div.col-sm-5
+              (for [crd (->> @deck :p1 :decklist (filter #(= (:pos %) (:id p))))]
+                (cardimgwrap :p1 crd))
             ; Command Icons  
-              [:div.command
-                (for [n (range (->> @deck :decklist (filter #(= (:pos %) (:id p))) (map :command_icons) (remove nil?) (reduce +)))]
+              [:div.commandl
+                (for [n (range (->> @deck :p1 :decklist (filter #(= (:pos %) (:id p))) (map :command_icons) (remove nil?) (reduce +)))]
                   ^{:key n}[:span [:i.fas.fa-gavel.fa-sm.command-icon]])]]
           ; Planets  
-            [:div.col-sm-4
-              [:div [:img.card-planet {:src (devimg (:img p)) 
-                                    :alt (if (:revealed p) (:name p) "Planet")
-                                    :on-mouse-over #(swap! appstate assoc :img (:img p))}]]]
+            [:div.col-sm-2
+              [:div [:img.whc-card-planet {:src (devimg (if (:revealed p) (:img p) "/img/cardback-planet.png")) 
+                                        :alt (if (:revealed p) (:name p) "Planet")
+                                        :on-mouse-over #(swap! appstate assoc :img (:img p))}]]]
           ; P2 deployment 
-            [:div.col-sm-4]]))))
+            [:div.col-sm-5
+            ; Command Icons  
+              [:div.commandr
+                (for [n (range (->> @deck :p2 :decklist (filter #(= (:pos %) (:id p))) (map :command_icons) (remove nil?) (reduce +)))]
+                  ^{:key n}[:span [:i.fas.fa-gavel.fa-sm.command-icon]])]
+              (for [crd (->> @deck :p2 :decklist (filter #(= (:pos %) (:id p))))]
+                (cardimgwrap :p2 crd))]]))))
           
 (defn board-log []
   [:div#log
@@ -240,11 +252,20 @@
   [:div.container-fluid
     [:div.row
       [:div.col-sm-9
-        [hq]
+        [:ul.nav.nav-tabs
+          [:li.nav-item
+            [:a.nav-link.active {:href "#p1hq" :data-toggle "tab" :role "tab"} "P1"]]
+          [:li.nav-item
+            [:a.nav-link {:href "#p2hq" :data-toggle "tab" :role "tab"} "P2"]]]
+        [:div.tab-content
+          [:div#p1hq.tab-pane.active
+            [hq :p1]]
+          [:div#p2hq.tab-pane
+            [hq :p2]]]
         (board-planets)]
       [:div.col-sm-3
         [:div.row
-          [:div.cardimg.w-100 [:img.img-fluid.float-right {:hidden (nil? (:img @appstate)) :src (devimg (:img @appstate))}]]]
+          [:div.whc-cardimg.w-100 [:img.img-fluid.float-right {:hidden (nil? (:img @appstate)) :src (devimg (:img @appstate))}]]]
         [:div.row
           (board-log)]]]
     [ctxmenu]
